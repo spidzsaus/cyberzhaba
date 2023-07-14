c7a25175b7299069fa48b54df573a3c92f0dff77
Sp1dz <spidepog@gmail.com>
2023-07-14 18:49:26 +0300

    Changed get_message, get_role and get_emoji methods of a ReactionRole entity

--- app/entities/reactionroles.py
+++ app/entities/reactionroles.py
@@ -3,7 +3,7 @@ import discord
 from app.db.models import SqlReactionRole
 from app.db import database
 from app.exceptions import AlreadyExists
-
+from app.helper_tools import async_none_on_catch
 
 class ReactionRole:
     id: int
@@ -13,14 +13,22 @@ class ReactionRole:
     role_id: int
 
     @classmethod
+    def by_id(cls, rrid: int):
+        db_sess = database.session()
+        reaction_role = db_sess.query(SqlReactionRole).filter(
+            SqlReactionRole.id == rrid
+        ).first()
+        return reaction_role and cls(reaction_role)
+
+    @classmethod
     def search(cls, message: discord.Message, reaction: discord.Emoji):
         db_sess = database.session()
         reaction_role = db_sess.query(SqlReactionRole).filter(
             SqlReactionRole.channel_id == message.channel.id,
             SqlReactionRole.message_id == message.id,
             SqlReactionRole.reaction_id == reaction.id
-        )
-        return reaction_role
+        ).first()
+        return reaction_role and cls(reaction_role)
 
     @classmethod
     def create(cls, message: discord.Message, reaction: discord.Emoji, role: discord.Role):
@@ -48,11 +56,20 @@ class ReactionRole:
         self.reaction_id = reaction_role.reaction_id
         self.role_id = reaction_role.role_id
 
-    async def get_message(self, client: discord.Client):
-        return await client.fetch_channel(self.channel_id).fetch_message(self.message_id)
+    @async_none_on_catch(discord.NotFound)
+    async def get_message(self, client: discord.Client) -> discord.Message:
+        channel = await client.fetch_channel(self.channel_id)
+        return await channel.fetch_message(self.message_id)
 
-    async def get_role(self, client: discord.Client):
-        return await client.fetch_channel(self.channel_id).guild.get_role(self.role_id)
+    @async_none_on_catch(discord.NotFound)
+    async def get_role(self, client: discord.Client) -> discord.Role:
+        channel = await client.fetch_channel(self.channel_id)
+        return channel.guild.get_role(self.role_id)
+
+    @async_none_on_catch(discord.NotFound)
+    async def get_emoji(self, client: discord.Client) -> discord.Emoji:
+        channel = await client.fetch_channel(self.channel_id)
+        return channel.guild.get_emoji(self.reaction_id)
 
     def __bool__(self):
         return True
