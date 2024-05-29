4754e8004a53172f083278536207c7983ec34b6c
Ilya Mikheev <me@ilyamikcoder.com>
2024-05-29 19:29:30 +0200

    run database migrations on startup

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import asyncio
 
+from alembic.config import Config
+from alembic import command
+
 from app import main
 
 if __name__ == '__main__':
+    # run database migrations
+    alembic_cfg = Config('./alembic.ini')
+    alembic_cfg.attributes['configure_logger'] = False
+    command.upgrade(alembic_cfg, 'head')
+
     asyncio.run(main())
